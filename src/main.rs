a685aea7db7b4aab3ce6816d90d3cc107d26ddd6
KangSinWon <wnfansl788@gmail.com>
2024-01-28 22:11:22 +0900

    chapter4 source code written by rust

---
+++ src/main.rs
@@ -0,0 +1,328 @@
+use std::{collections::HashMap, error::Error, fs};
+
+struct Lox {
+    had_error: bool,
+}
+
+impl Lox {
+    fn error(&mut self, line: usize, message: &str) {
+        self.report(line, String::from(""), message);
+    }
+
+    fn report(&mut self, line: usize, on: String, message: &str) {
+        println!("[line: {}] Error {}: {}", line, on, message);
+        self.had_error = true;
+    }
+}
+
+static mut LOX: Lox = Lox { had_error: false };
+
+struct Scanner<'a> {
+    source: String,
+    tokens: Vec<Token>,
+
+    start: usize,
+    current: usize,
+    line: usize,
+
+    keywords: HashMap<&'a str, TokenType>,
+}
+
+impl Scanner<'_> {
+    pub fn scan_tokens(&mut self) -> Vec<Token> {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token()
+        }
+
+        self.tokens.push(Token {
+            token_type: TokenType::Eof,
+            lexeme: "".to_string(),
+            literal: "".to_string(),
+            line: self.line,
+        });
+        self.tokens.clone()
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    fn scan_token(&mut self) {
+        let c = self.advance();
+        match c {
+            '(' => self.add_token(TokenType::LeftParen, "".to_string()),
+            ')' => self.add_token(TokenType::RightParen, "".to_string()),
+            '{' => self.add_token(TokenType::LeftBrace, "".to_string()),
+            '}' => self.add_token(TokenType::RightBrace, "".to_string()),
+            ',' => self.add_token(TokenType::Comma, "".to_string()),
+            '.' => self.add_token(TokenType::Dot, "".to_string()),
+            '-' => self.add_token(TokenType::Minus, "".to_string()),
+            '+' => self.add_token(TokenType::Plus, "".to_string()),
+            ';' => self.add_token(TokenType::Semicolon, "".to_string()),
+            '*' => self.add_token(TokenType::Star, "".to_string()),
+            '!' => {
+                if self.match_token('=') {
+                    self.add_token(TokenType::BangEqual, "".to_string());
+                } else {
+                    self.add_token(TokenType::Bang, "".to_string())
+                }
+            }
+            '=' => {
+                if self.match_token('=') {
+                    self.add_token(TokenType::EqualEqual, "".to_string());
+                } else {
+                    self.add_token(TokenType::Equal, "".to_string())
+                }
+            }
+            '<' => {
+                if self.match_token('=') {
+                    self.add_token(TokenType::LessEqual, "".to_string());
+                } else {
+                    self.add_token(TokenType::Less, "".to_string())
+                }
+            }
+            '>' => {
+                if self.match_token('=') {
+                    self.add_token(TokenType::GreaterEqual, "".to_string());
+                } else {
+                    self.add_token(TokenType::Greater, "".to_string())
+                }
+            }
+            '/' => {
+                if self.match_token('/') {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::Slash, "".to_string())
+                }
+            }
+            ' ' => (),
+            '\r' => (),
+            '\t' => (),
+            '\n' => self.line += 1,
+            '"' => self.scan_string(),
+            _ => {
+                if self.is_digit(c) {
+                    self.number();
+                } else if self.is_alpha(c) {
+                    self.identifier();
+                } else {
+                    unsafe { LOX.error(self.line, "Unexpected character.") }
+                }
+            }
+        }
+    }
+
+    fn advance(&mut self) -> char {
+        let c = self.source.chars().nth(self.current).unwrap();
+        self.current += 1;
+
+        c
+    }
+
+    fn match_token(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+
+        if self.source.chars().nth(self.current).unwrap() != expected {
+            return false;
+        }
+
+        self.current += 1;
+        true
+    }
+
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            return '\0';
+        }
+
+        self.source.chars().nth(self.current).unwrap()
+    }
+
+    fn peek_next(&self) -> char {
+        if self.current + 1 >= self.source.len() {
+            return '\0';
+        }
+
+        self.source.chars().nth(self.current + 1).unwrap()
+    }
+
+    fn scan_string(&mut self) {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            unsafe {
+                LOX.error(self.line, "Unterminated string.");
+            }
+            return;
+        }
+
+        let value = self.source[self.start + 1..self.current - 1].to_string();
+        self.add_token(TokenType::String, value);
+    }
+
+    fn is_digit(&self, c: char) -> bool {
+        c >= '0' && c <= '9'
+    }
+
+    fn number(&mut self) {
+        while self.is_digit(self.peek()) {
+            self.advance();
+        }
+
+        if self.peek() == '.' && self.is_digit(self.peek_next()) {
+            self.advance();
+
+            while self.is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        let value = self.source[self.start..self.current].to_string();
+        self.add_token(TokenType::Number, value);
+    }
+
+    fn is_alpha(&self, c: char) -> bool {
+        (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
+    }
+
+    fn is_alpah_numeric(&self, c: char) -> bool {
+        self.is_alpha(c) || self.is_digit(c)
+    }
+
+    fn identifier(&mut self) {
+        while self.is_alpah_numeric(self.peek()) {
+            self.advance();
+        }
+
+        let text = self.source[self.start..self.current].to_string();
+
+        match self.keywords.get(text.as_str()) {
+            Some(t) => self.add_token(t.clone(), text),
+            None => self.add_token(TokenType::Identifier, "".to_string()),
+        }
+    }
+
+    fn add_token(&mut self, token_type: TokenType, literal: String) {
+        let text = self.source[self.start..self.current].to_string();
+        self.tokens.push(Token {
+            token_type,
+            lexeme: text,
+            literal,
+            line: self.line,
+        });
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: String, // Object?
+    line: usize,
+}
+
+#[derive(Debug, Clone)]
+enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
+
+fn run(source: String) {
+    let keywords = HashMap::from([
+        ("false", TokenType::False),
+        ("for", TokenType::For),
+        ("fun", TokenType::Fun),
+        ("if", TokenType::If),
+        ("nil", TokenType::Nil),
+        ("or", TokenType::Or),
+        ("print", TokenType::Print),
+        ("return", TokenType::Return),
+        ("super", TokenType::Super),
+        ("this", TokenType::This),
+        ("true", TokenType::True),
+        ("var", TokenType::Var),
+        ("while", TokenType::While),
+    ]);
+
+    let mut scanner = Scanner {
+        source,
+        tokens: Vec::new(),
+        start: 0,
+        current: 0,
+        line: 1,
+        keywords,
+    };
+
+    let tokens = scanner.scan_tokens();
+    for token in tokens.iter() {
+        println!("{:?}", token);
+    }
+}
+
+fn read_file(path: String) -> Result<(), Box<dyn Error>> {
+    let source = fs::read_to_string(path)?.parse()?;
+    run(source);
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let res = read_file(String::from("lox.txt"));
+
+    res
+}
